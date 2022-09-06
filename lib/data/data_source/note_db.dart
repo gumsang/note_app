2b55b98b99111408270af387abe15209beb0e93b
eunsang.kim <gomuzom@naver.com>
2022-09-06 20:26:04 +0900

    feat: repository 추상화 및 구현

--- lib/data/data_source/note_db.dart
+++ lib/data/data_source/note_db.dart
@@ -1,1 +1,40 @@
-class NoteDb {}
+import 'package:sqflite/sqlite_api.dart';
+
+import '../../domain/model/note.dart';
+
+class NoteDb {
+  Database db;
+
+  NoteDb(this.db);
+
+  Future<Note?> getNoteById(int id) async {
+    final List<Map<String, dynamic>> maps = await db.query(
+      'note',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return Note.fromJson(maps.first);
+    }
+    return null;
+  }
+
+  Future<List<Note>> getNotes() async {
+    final maps = await db.query('note');
+    return maps.map((e) => Note.fromJson(e)).toList();
+  }
+
+  Future<void> insertNote(Note note) async {
+    await db.insert('note', note.toJson());
+  }
+
+  Future<void> updateNote(Note note) async {
+    await db.update(
+      'note',
+      note.toJson(),
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
+  }
+}
